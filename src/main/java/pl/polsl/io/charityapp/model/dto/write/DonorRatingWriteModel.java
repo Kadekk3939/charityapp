90d4e10794ed2dd94195bbb0ac3dd59169f39300
Kadekk3939 <kadekk3939@gmail.com>
2022-12-12 15:57:04 +0100

    Add few DTO models

---
+++ src/main/java/pl/polsl/io/charityapp/model/dto/write/DonorRatingWriteModel.java
@@ -0,0 +1,14 @@
+package pl.polsl.io.charityapp.model.dto.write;
+
+import lombok.Data;
+
+@Data
+public class DonorRatingWriteModel {
+
+    private String donorLogin;
+
+    private String charityActionName;
+
+    private boolean rating;
+
+}
