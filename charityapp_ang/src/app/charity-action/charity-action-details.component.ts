0188c8fbcab9fbaccf67d19a06e9c953a40cbdd7
MaciejMaciejewski <maciekageof@gmail.com>
2023-01-14 19:42:58 +0100

    Added button show only when user login is bbb(benefactor) for presentation purposes only

--- charityapp_ang/src/app/charity-action/charity-action-details.component.ts
+++ charityapp_ang/src/app/charity-action/charity-action-details.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CharityAction } from "./charity-action";
 import { CharityActionService } from "./charity-action.service";
 import { RouterModule } from '@angular/router';
+import { UserServiceService } from "../user-service.service";
+import { AppService } from "../app.service";
 
 
 @Component({
@@ -15,10 +17,12 @@ import { RouterModule } from '@angular/router';
   export class CharityActionDetailsComponent implements OnInit {
     public charityAction: CharityAction;
     name:string;
+    public login:string;
     private sub:any;
-    constructor(private charityActionService: CharityActionService,private router:Router,private routeP: ActivatedRoute){}
+    constructor(private charityActionService: CharityActionService,private router:Router,private routeP: ActivatedRoute,private app:AppService){}
         
     ngOnInit(): void {
+        this.login = this.app.login;
         this.sub = this.routeP.params.subscribe(params =>
             this.name = params['name'])
             this.charityActionService.getCharityActionByName(this.name).subscribe(
@@ -33,5 +37,14 @@ import { RouterModule } from '@angular/router';
     }
     public routeR(){
         this.router.navigateByUrl('/user');
+    }
+    public routeA(){
+        this.router.navigateByUrl('/applay');
+    }
+    public isBenecient():boolean{
+        if(this.login == "bbb"){
+            return true;
+        }      
+        return false;
     }
 }
