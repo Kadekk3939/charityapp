d632cff85ff665799f90ad9628b1eee7fbba5531
Jolanta Lachman <jolalac346@student.polsl.pl>
2023-02-15 08:04:03 +0100

    Fix error

--- charityapp_ang/src/app/benefactors-applications/benefactors-applications.component.ts
+++ charityapp_ang/src/app/benefactors-applications/benefactors-applications.component.ts
@@ -1,12 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CharityAction } from "./charity-action";
-import { CharityActionService } from "./charity-action.service";
-import { HttpErrorResponse } from "@angular/common/http";
-//import {Router} from "@angular/router";
-import { NgForm } from "@angular/forms";
-import { Router } from '@angular/router';
-import { User } from '../user';
-import { AppService } from '../app.service';
+import { Component} from '@angular/core';
+
 @Component({
   selector: 'app-benefactors-applications',
   templateUrl: './benefactors-applications.component.html',
